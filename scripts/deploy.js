2eb84beed72d7ac3f13a476e316782bf376d3cb5
tur461 <tur461.er@gmail.com>
2024-02-20 08:24:10 +0530

    chore: work on addLiquidityEth and removeLiquidityEth

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,66 +16,66 @@ async function main() {
   // 1. Retrieve signers from the ethers provider.
   const [owner] = await ethers.getSigners();
   console.log(`Deploying contracts with the account: ${owner.address}`);
-
+  
   // 2. Initialize a new contract factory for the Uniswap V2 Factory.
   // This factory requires the ABI and bytecode from the factoryArtifact.
   const Factory = new ContractFactory(
-    factoryArtifact.abi,
-    factoryArtifact.bytecode,
-    owner
+  factoryArtifact.abi,
+  factoryArtifact.bytecode,
+  owner
   );
-
+  
   // 3. Use the initialized factory to deploy a new Factory contract.
   // The deployment is signed by the owner.
   const factory = await Factory.deploy(owner.address);
-
+  
   // 4. After deployment, retrieve the address of the newly deployed Factory contract.
   const factoryAddress = await factory.getAddress();
   console.log(`Factory deployed to ${factoryAddress}`);
-
+  
   // 5. Initialize a contract factory specifically for the Tether (USDT) token.
   const USDT = await ethers.getContractFactory("Tether", owner);
-
+  
   // 6. Deploy the USDT contract using the above-initialized factory.
   const usdt = await USDT.deploy();
-
+  
   // 7. Get the address of the deployed USDT contract.
   const usdtAddress = await usdt.getAddress();
   console.log(`USDT deployed to ${usdtAddress}`);
-
+  
   // 8. Similarly, initialize a contract factory for the UsdCoin (USDC) token.
   const USDC = await ethers.getContractFactory("UsdCoin", owner);
-
+  
   // 9. Deploy the USDC contract.
   const usdc = await USDC.deploy();
-
+  
   // 10. Get the address of the deployed USDC contract.
   const usdcAddress = await usdc.getAddress();
   console.log(`USDC deployed to ${usdcAddress}`);
-
-
+  
+  
   const GTH = await ethers.getContractFactory('GTH', owner);
   // deploy the GTH contract
   const gth = await GTH.deploy();
-
+  
   const gthAddr = await gth.getAddress();
   console.log(`GTH deployed to ${gthAddr}`);
   
   
-
+  
   const isPaused = await gth.paused();
   console.log(`GTH Paused ?: ${isPaused}`);
-
+  
   let txn;
-
+  
   /**
-   * Now that we have deployed the Factory contract and the two ERC20 tokens,
-   * we can deploy the Router contract.
-   * The Router contract requires the address of the Factory contract and the WETH9 contract.
-   * The WETH9 contract is a wrapper for the ETH token.
-   * But prior to that, we need to mint some USDT and USDC tokens to the owner. Lets do that first.
-   */
-
+  * Now that we have deployed the Factory contract and the two ERC20 tokens,
+  * we can deploy the Router contract.
+  * The Router contract requires the address of the Factory contract and the WETH9 contract.
+  * The WETH9 contract is a wrapper for the ETH token.
+  * But prior to that, we need to mint some USDT and USDC tokens to the owner. Lets do that first.
+  */
+  
   // 11. Mint 1000 USDT tokens to the owner.
   txn = await usdt.connect(owner).mint(owner.address, ethers.parseEther("1000"));
   console.log(`usdt minted to owner ${await usdt.balanceOf(owner)}`)
@@ -91,116 +91,135 @@ async function main() {
     const isPaused = await gth.paused();
     console.log(`GTH Paused ?: ${isPaused}`);
   }
-
+  
   // 13. Utilizing the Factory contract, create a trading pair using the addresses of USDT and USDC.
   // const tx1 = await factory.createPair(usdtAddress, usdcAddress);
   const tx1 = await factory.createPair(usdtAddress, gth);
-
+  
   // 14. Wait for the transaction to be confirmed on the blockchain.
   await tx1.wait();
   console.log('usdt-gth pair created')
-
+  
   // 15. Retrieve the address of the created trading pair from the Factory contract.
   // const pairAddress = await factory.getPair(usdtAddress, usdcAddress);
   const pairAddress = await factory.getPair(usdtAddress, gthAddr);
   console.log(`Pair deployed to ${pairAddress}`);
-
+  
   // 16. Initialize a new contract instance for the trading pair using its address and ABI.
   const pair = new Contract(pairAddress, pairArtifact.abi, owner);
-
+  
   // 17. Query the reserves of the trading pair to check liquidity.
   let reserves = await pair.getReserves();
   console.log(`Reserves: ${reserves[0].toString()}, ${reserves[1].toString()}`);
-
+  
   // 18. Initialize a new contract factory for the WETH9 contract.
   const WETH = new ContractFactory(WETH9.abi, WETH9.bytecode, owner);
   const weth = await WETH.deploy();
   const wethAddress = await weth.getAddress();
   console.log(`WETH deployed to ${wethAddress}`);
-
+  
   // 19. Initialize a new contract factory for the Router contract.
   const Router = new ContractFactory(
-    routerArtifact.abi,
-    routerArtifact.bytecode,
-    owner
+  routerArtifact.abi,
+  routerArtifact.bytecode,
+  owner
   );
-
+  
   // 20. Deploy the Router contract using the above-initialized factory.
   const router = await Router.deploy(factoryAddress, wethAddress);
   const routerAddress = await router.getAddress();
   console.log(`Router deployed to ${routerAddress}`);
-
+  
   const MaxUint256 =
-    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
   
   console.log(`usdt approved for the router [before]: ${await usdt.allowance(owner, router)}`)
   const approveTx1 = await usdt.approve(routerAddress, MaxUint256);
   await approveTx1.wait();
   console.log(`usdt approved for the router [after]: ${await usdt.allowance(owner, router)}`)
-
+  
   console.log(`gth approved for the router [before]: ${await gth.allowance(owner, router)}`)
   // const approvalTx2 = await usdc.approve(routerAddress, MaxUint256);
   const approvalTx2 = await gth.approve(routerAddress, MaxUint256);
   await approvalTx2.wait();
   console.log(`gth approved for the router [after]: ${await gth.allowance(owner, router)}`)
-
+  
   const token0Amount = ethers.parseUnits("100");
   const token1Amount = ethers.parseUnits("100");
-
+  
   const lpTokenBalanceBefore = await pair.balanceOf(owner.address);
   console.log(
-    `LP tokens for the owner before: ${lpTokenBalanceBefore.toString()}`
+  `LP tokens for the owner before: ${lpTokenBalanceBefore.toString()}`
   );
-
+  
   const deadline = Math.floor(Date.now() / 1000) + 10 * 60;
+  const options = { value: token0Amount }
   const addLiquidityTx = await router
-    .connect(owner)
-    .addLiquidity(
-      usdtAddress,
-      // usdcAddress,
-      gthAddr,
-      token0Amount,
-      token1Amount,
-      0,
-      0,
-      owner,
-      deadline
+  .connect(owner)
+  .addLiquidityETH(
+    // usdtAddress,
+    // usdcAddress,
+    gthAddr,
+    // token0Amount,
+    token1Amount,
+    0,
+    0,
+    owner,
+    deadline,
+    options
     );
-  await addLiquidityTx.wait();
-  console.log('usdt-gth liquidity added')
+    const liq = await addLiquidityTx.wait();
+    
+    console.log('eth-gth liquidity added', routerArtifact.abi)
+      const dec = (abi, data) => ethers.AbiCoder.defaultAbiCoder().decode(
+        abi,
+        data
+    );
+  const logs = liq.logs//.find(event => event.event === 'Transfer');
+  const iface = new ethers.Interface(routerArtifact.abi);
+  // const parsed = iface.parseTransaction({data: liq.data, value: liq.value});
+  console.log(logs.map(log => log.topics[0].slice(0, 10)))
 
+  const pEvs = pairArtifact.abi.filter(j => j.type == 'event');
+  console.log('# of Event:', pEvs)
+  
+  console.log(dec([pEvs[5]], logs[1].data))
+  
   // Check LP token balance for the owner
   const lpTokenBalance = await pair.balanceOf(owner.address);
   console.log(`##LP tokens for the owner: ${lpTokenBalance.toString()}`);
-
+  
   reserves = await pair.getReserves();
   console.log(`Reserves: ${reserves[0].toString()}, ${reserves[1].toString()}`);
-
   
-
-
+  
+  
+  
   // -------------------- LP MIGRATION --------------------
   
-
+  
   const LPMigration = await ethers.getContractFactory('LPMigration', owner);
   // deploy the GTH contract
   const lpMigration = await LPMigration.deploy(routerAddress, gthAddr);
   const lpMigrationAddr = await lpMigration.getAddress();
-
+  
+  const liquidity = await pair.balanceOf(owner);
+  console.log(`Total liquidity of Owner: ${liquidity}`)
   // give allowance to lpMigration contract
   console.log(`lp tokens approved to lpMigration [before]: ${await pair.allowance(owner, lpMigrationAddr)}`)
-  txn = await pair.approve(lpMigrationAddr, MaxUint256)
+  txn = await pair.approve(routerAddress, )
   await txn.wait()
   console.log(`lp tokens approved to lpMigration [after]: ${await pair.allowance(owner, lpMigrationAddr)}`)
   
   // make lpMigration contract owner of gth token contract
+  // for pause/unpause
   txn = await gth.connect(owner).transferOwnership(lpMigrationAddr)
   await txn.wait()
   console.log('Ownership of gth transferred to lpMigration contract')
-
+  
   const liquidateTxn = await lpMigration.connect(owner).liquidate(pair, deadline)
   await liquidateTxn.wait()
-
+  
   console.log("USDT_ADDRESS", usdtAddress);
   console.log("USDC_ADDRESS", usdcAddress);
   console.log("WETH_ADDRESS", wethAddress);
@@ -209,14 +228,15 @@ async function main() {
   console.log("PAIR_ADDRESS", pairAddress);
   console.log("LPMigration_ADDRESS", lpMigrationAddr);
 }
-
+      
 // This command is used to run the script using hardhat.
 // npx hardhat run --network localhost scripts/01_deployContracts.js
 
 // Executing the main function and handling possible outcomes.
 main()
-  .then(() => process.exit(0)) // Exiting the process if deployment is successful.
-  .catch((error) => {
-    console.error(error); // Logging any errors encountered during deployment.
-    process.exit(1); // Exiting the process with an error code.
-  });
+.then(() => process.exit(0)) // Exiting the process if deployment is successful.
+.catch((error) => {
+  console.error(error); // Logging any errors encountered during deployment.
+  process.exit(1); // Exiting the process with an error code.
+});
+        
